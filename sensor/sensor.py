69849558701229c46f3afc72496acab5563dd389
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-04-22 01:28:47 +1000

    Dimitris rebase plx?

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
 # MQTT Sensor
+import paho.mqtt.client as paho
+import time
 import Adafruit_DHT
-import paho.mqtt.publish as publish
+import time
+import json
+DHT_SENSOR = Adafruit_DHT.DHT11
+DHT_PIN = 2
+IP="192.168.1.126"
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
+def on_connect(client, userdata, flags, rc):
+    print("CONNACK received with code %d." % (rc))
+
+
+def on_publish(client, userdata, mid):
+    print("mid: "+str(mid))
+
+
+client = paho.Client()
+client.on_connect = on_connect
+client.on_publish = on_publish
+client.connect(IP, 1883)
+client.loop_start()
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
     if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-        publish.single("Greenhouse/temp", temperature, hostname="localhost")  #localhost for testing purposes only
-        publish.single("Greenhouse/humidity", humidity, hostname="localhost")
+        payload = { "temperature": temperature, "humidity": humidity }
+        (rc, mid) = client.publish("greenhouse/sensor", json.dumps(payload), qos=1)
     else:
         print("Failed to retrieve data from humidity sensor")
+    time.sleep(300)
