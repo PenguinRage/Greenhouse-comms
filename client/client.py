d387a4a61217ff77b8e2c35c7f462596b3711a34
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-05-31 12:08:36 +1000

    need another beer

--- client/client.py
+++ client/client.py
@@ -23,5 +23,5 @@ def on_message(client, userdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("server", 1883, 60)
+client.connect("server.local", 1883, 60)
 client.loop_forever()
