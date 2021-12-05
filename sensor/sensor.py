accf66bba9828c16340743362acccfb838f66af6
Ian Cleasby <3924194+PenguinRage@users.noreply.github.com>
2021-12-05 23:36:07 +1100

    this should fix it

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -6,24 +6,32 @@ import Adafruit_DHT
 import time
 import json
 import os
+import logging
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 2
 
 from influxdb import InfluxDBClient
+from influxdb.exceptions import InfluxDBServerError
+
+
 client = InfluxDBClient(host='server.local', port=8086, username=os.environ['INFLUXDB_USER'], password=os.environ['INFLUXDB_PWD'])
-client.switch_database('greenhouse')
+client.switch_database('home_assistant')
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     if humidity is not None and temperature is not None:
-        payload = [ 
-                { 
-                    "measurement": "sensor", 
+        payload = [
+                {
+                    "measurement": "sensors",
+                    "tags": { "environment": "greenhouse" },
                     "fields": { "temperature": temperature, "humidity": humidity }
                 }
             ]
-        client.write_points(payload)
+        try:
+            client.write_points(payload)
+        except (InfluxDBServerError):
+            logging.error('Failed to send metrics to influxdb')
     else:
         print("Failed to retrieve data from humidity sensor")
     time.sleep(60)
